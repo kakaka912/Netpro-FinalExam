641a8660478ca839df27faee203cec7fb72b33cc
kakaka912 <delaware20050912@gmail.com>
2026-07-13 16:38:26 +0900

    scenario control

--- Server.js
+++ Server.js
@@ -27,7 +27,6 @@ function getTodayString() {
 const todayText = getTodayString();
 
 const scenarioD_WakeUp = [
-    { speaker: "System", text: "" },
     { speaker: "System", text: "※チャットは通信相手に見えません" },
     { speaker: "System", text: "【Cランク地区9-505棟】" },
     { speaker: "Ai Min", text: "起床時刻です。" },
@@ -73,7 +72,6 @@ const scenarioD_CallStart = [
 ];
 
 const scenarioP_WakeUp = [
-    { speaker: "System", text: "" },
     { speaker: "System", text: "※チャットは通信相手に見えません" },
     { speaker: "System", text: "【Bランク地区-avocadoビルディング】" },
     { speaker: "Ai Min", text: "おはようございます！ 本日も素晴らしい朝がやってきました。" },
